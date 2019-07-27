ed3166149a730aaca76c97be34e614e11d226f24
Olena Ageyeva <oageyeva@Commonwealth.com>
2019-07-27 15:59:44 -0400

    validation added

--- src/app/user-setting-form/user-setting-form.component.ts
+++ src/app/user-setting-form/user-setting-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from 'src/data/user-settings';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-setting-form',
@@ -9,18 +10,26 @@ import { UserSettings } from 'src/data/user-settings';
 export class UserSettingFormComponent implements OnInit {
 
   originalUserSettings: UserSettings ={
-    name: "Milton",
-    emailOffers: true,
-    interfaceStyle: 'dark',
-    subscriptionType: 'Annual',
-    notes: 'some notes'
-  }
+    name: null,
+    emailOffers: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null
+    }
 
   userSettings: UserSettings={... this.originalUserSettings};
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm){
+    console.log('in onSubmit: ', form.valid)
+  }
+
+  onBlur(field: NgModel){
+    console.log('in onBlur: ', field.valid)
   }
 
 }
